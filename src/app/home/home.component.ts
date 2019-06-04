949b1208ecd4a2361d9b729691d209a2ab993aa5
danny9mvp <danny9mvp@gmail.com>
2019-06-04 00:18:14 -0500

    Agregado un servicio para consumir apis de desarrollo en imgur con el fin de alojar las fotos allí.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,58 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef} from '@angular/core';
 import { Router } from '@angular/router';
-
+import { ReporteService } from '../services/reporte.service';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ImgUrService } from '../services/img-ur.service';
 @Component({
   selector: 'app-child',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {  
+  formReporte: FormGroup;  
   nick:string;
-  constructor(private router:Router) { }
+  reportes:any;
+  foto:File=null;   
+
+  constructor(private reporteService:ReporteService, private imgUrService:ImgUrService ,private router:Router, 
+    private formBuilder:FormBuilder) { }    
+
+  onFileChange(event) {
+     this.foto = event.target.files[0]; // <--- File Object for future use.     
+  }
+
+  onSubmit(){    
+    console.log(this.foto);    
+    this.imgUrService.postImgUr(this.foto).subscribe(data=>{
+      console.log(data);      
+      var reporte = {
+        titulo:this.formReporte.controls['titulo'].value, descripcion:this.formReporte.controls['descripcion'].value,
+        usuario:this.nick, fecha:new Date().toUTCString(), estado: 'En espera', ubicacion: 1,foto: data.data.link
+      };
+      this.reporteService.crearReporte(reporte).subscribe(data=>{
+        console.log(data);
+      },
+      error=>{
+        console.log(error);
+      });
+    },
+    error=>{
+      console.log(error);
+    });      
+  }
 
   ngOnInit() {  	
   	if(window.sessionStorage)
     	this.nick = sessionStorage.getItem("nick");
+    this.reporteService.listarReportes().subscribe(data =>{
+    	this.reportes = data;
+    	console.log(data);
+    },
+    err=>{
+    	console.log(err);
+    });
+    this.formReporte = this.formBuilder.group({titulo:['', Validators.required], descripcion:['', Validators.required], 
+      foto:[null, Validators.required]});    
   }
 
 }
